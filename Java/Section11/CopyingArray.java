9d947c8da9e03d1055362eb1ea8002144b52c1b4
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-03-26 23:12:20 +0530

    Added array copying functionality
    Implemented main method to copy and display array elements.

--- Java/Section11/CopyingArray.java
+++ Java/Section11/CopyingArray.java
@@ -1,3 +1,17 @@
 public class CopyingArray {
-    
+     public static void main(String args[])
+    {
+        int a[]={1,2,3,4,5,6,7,8,9,90};
+        int b[]=new int[10];
+
+        for(int x:a)
+            System.out.print(x+",");
+        System.out.println(" ");
+        for(int i=0;i<a.length;i++)
+        {
+            b[i]=a[i];
+        }
+        for(int x:b)
+            System.out.print(x+",");
+    }
 }
