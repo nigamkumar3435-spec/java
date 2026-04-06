40ce00b214718938fd9335d4ceb0b9fa23aa24b9
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-06 21:41:06 +0530

    code for bitwise operator

---
+++ Java/Section5/Bitwise.java
@@ -0,0 +1,19 @@
+/*class Bitwise
+{
+	public static void main(String args[])
+	{
+		int x=10,y=6,z;
+		z=x&y;
+		System.out.println(z);
+	}
+}
+*/
+class Bitwise
+{
+	public static void main(String args[])
+	{
+		int x=10,y=6,z;
+		z=x|y;
+		System.out.println(z);
+	}
+}		
