888cc38edc6f374721625f5cbbce6b349348b7c2
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-08 23:14:20 +0530

    Create Incdec.java for increment and calculation example
    Add Incdec.java to demonstrate increment and calculation.

---
+++ Java/Section5/Incdec.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class Incdec{
+public static void main(String args[])
+{
+int l=4,b=6,h;
+h=2*l++ + 3*++l;
+
+System.out.println(l);
+System.out.println(b);
+System.out.println(h);
+}
+}
