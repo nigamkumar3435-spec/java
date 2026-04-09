060e9cd745d64f18d34c9e851b7295a94b827f9a
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-09 22:53:55 +0530

    Implement Circle and Cylinder classes using Inheritance

--- Java/Sectiion15/Inheritance1.java
+++ Java/Sectiion15/Inheritance1.java
@@ -1,1 +1,37 @@
+class Circle
+{
+    public double radius;
+    
+    public double area()
+    {
+        return Math.PI*radius*radius;
+    }
+    public double perimeter()
+    {
+        return Math.PI*2*radius;
+    }
+}
+class Cylinder extends Circle
+{
+    public double height;
+    
+    public double volume()
+    {
+        return area()*height;
+    }
+}
+public class Inheritance1 
+{
+    public static void main(String[] args) 
+    {
+       Cylinder c2=new Cylinder();
+       
+       c2.radius=5;
+       c2.height=6;
+       
+       System.out.println("Area1:"+c2.area());
+       System.out.println("Area2:"+c2.volume());
+    }
+    
+}
 
