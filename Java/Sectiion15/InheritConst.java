b1d41f769af4d82756c394d41cbd0728edb06e74
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-11 15:15:22 +0530

    Implement inheritance with constructors in Java

--- Java/Sectiion15/InheritConst.java
+++ Java/Sectiion15/InheritConst.java
@@ -1,1 +1,31 @@
-
+class Parent
+{
+    public Parent()
+    {
+        System.out.println("Parent Constructor");
+    }
+}
+class Child extends Parent
+{
+    public Child()
+    {
+        System.out.println("Child Constructor");
+    }
+}
+class GrandChild extends Child
+{
+    public GrandChild()
+    {
+        System.out.println("GrandChild Constructor");
+    }
+}
+public class InheritConst
+{
+    public static void main(String[] args) 
+    {
+//        Parent p=new Parent();
+//        Child c=new Child();
+          GrandChild g=new GrandChild();
+    }
+    
+}
