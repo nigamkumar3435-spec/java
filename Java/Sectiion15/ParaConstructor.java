8b623eb5694dd478998c2de1dc270a1ef718d6ff
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-11 15:12:45 +0530

    Add Parent and Child classes with constructors

--- Java/Sectiion15/ParaConstructor.java
+++ Java/Sectiion15/ParaConstructor.java
@@ -1,1 +1,36 @@
-
+class Parent
+{
+    Parent()
+    {
+        System.out.println("Non-Param of Parent");
+    }
+    Parent(int x)
+    {
+        System.out.println("Param of Parent"+x);
+    }
+}
+class Child extends Parent
+{
+    Child()
+    {
+        System.out.println("Non-Param of Child");
+    }
+    Child(int y)
+    {
+        System.out.println("Param of Child");
+    }
+    Child(int x, int y)
+    {
+        super(x);
+        System.out.println("2Param of Child"+y);
+    }
+}
+public class ParaConstructor 
+{
+    public static void main(String[] args) 
+    {
+//        Child c=new Child();
+//        Child c=new Child(2);
+        Child c=new Child(1,2);
+    }    
+}
