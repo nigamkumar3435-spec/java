31aef46cd88b602697b79ffb789d529693a964d9
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-12 18:44:46 +0530

    Add Rectangle and Cuboid classes with volume method

--- Java/Sectiion15/SuperConst.java
+++ Java/Sectiion15/SuperConst.java
@@ -1,1 +1,48 @@
+class Rectangle
+{
+    int length;
+    int breadth;
+    
+    Rectangle()
+    {
+        length=breadth=1;
+    }
+    Rectangle(int l, int b)
+    {
+        length=l;
+        breadth=b;
+    }
+}
+class Cuboid extends Rectangle
+{
+    int height;
+    
+    Cuboid()
+    {
+        height=1;
+    }
+    Cuboid(int h)
+    {
+        height=h;
+    }
+    Cuboid(int l, int b,int h)
+    {
+        super(l,b);
+        height=h;
+    }
+    int volume()
+    {   
+        return length*breadth*height;
+    }
+}
+public class SuperConst 
+{
+    public static void main(String[] args) 
+    {
+//        Cuboid c=new Cuboid();
+//        Cuboid c=new Cuboid(10);
+        Cuboid c=new Cuboid(10,5,2);
+        System.out.println(c.volume());
+    }   
+}
 
