5ad24b053b6f89ce0e20548202b617befa66bd20
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-13 23:06:22 +0530

    Implement Super Keyword with display method

--- Java/Sectiion15/SuperKey.java
+++ Java/Sectiion15/SuperKey.java
@@ -1,1 +1,40 @@
+class Rectangle
+{
+    int length;
+    int breadth;
+    
+    int x=10;
+    
+    Rectangle(int length, int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+}
+class Cuboid extends Rectangle
+{
+    int height;
+    int x=20;
+    
+    Cuboid(int l,int b, int h)
+    {
+        super(l,b);
+        height=h;
+    }
+    
+    void display()
+    {
+        System.out.println(super.x);
+        System.out.print(x);
+    }
+}
+public class SuperKey 
+{
+    public static void main(String[] args) 
+    {
+        Cuboid c=new Cuboid(100,200,300);
+        c.display();
+    }
+    
+}
 
