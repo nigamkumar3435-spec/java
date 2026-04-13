d424c3868dada6f84d8704e406095def6eb6c6a3
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-13 23:22:11 +0530

    Implement method overriding in Super and Sub classes

--- Java/Sectiion15/Overriding.java
+++ Java/Sectiion15/Overriding.java
@@ -1,1 +1,30 @@
+class Super
+{
+    public void display()
+    {
+        System.out.println("Super Class Display 1");
+    }
+}
+class Sub extends Super
+{
+    @Override
+    public void display()
+    {
+        System.out.println("Sub Class Display 2");
+    }
+}
+public class Overriding 
+{
+    public static void main(String[] args) 
+    {
+        Super sup=new Super();
+        sup.display();
+        
+        Sub s=new Sub();
+        s.display();
+        
+        Super sub=new Sub();
+        sub.display();
+    }   
+}
 
