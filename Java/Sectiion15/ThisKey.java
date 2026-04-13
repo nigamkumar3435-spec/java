752fda20bc2b80caa7c1b42219d3a27daf48e014
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-13 22:50:50 +0530

    Implement This keyword in class
    Added Rectangle class with constructor and display method.

--- Java/Sectiion15/ThisKey.java
+++ Java/Sectiion15/ThisKey.java
@@ -1,1 +1,30 @@
-
+class Rectangle
+{
+    int length;
+    int breadth;
+    
+    Rectangle(int length, int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+    
+    int display()
+    {
+        System.out.println("Length: "+this.length);
+        System.out.println("Breadth: "+this.breadth);
+        
+        return 0;
+    }
+}
+public class ThisKey
+{
+    public static void main(String[] args) 
+    {
+//        Rectangle r1=new Rectangle();
+        Rectangle r2=new Rectangle(10,20);
+        
+        System.out.println(r2.display());
+    }
+    
+}
