f71ba442d546a99d1296d5a617f0351b98083376
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-19 23:26:49 +0530

    Implement Car and LuxuryCar classes with method overriding

--- Java/Sectiion15/Overriding2.java
+++ Java/Sectiion15/Overriding2.java
@@ -1,1 +1,38 @@
-
+class Car
+{
+    public void start()
+    {
+        System.out.println("Car Started");
+    }
+    public void accelerate()
+    {
+        System.out.println("Car is accelerated");
+    }
+    public void changeGear()
+    {
+        System.out.println("Car gear changed");
+    }
+}
+class LuxuryCar extends Car
+{
+    public void changeGear()
+    {
+        System.out.println("Automatic Gear");
+    }
+    public void openRoof()
+    {
+        System.out.println("Sun Roof is Opened");
+    }
+}
+public class Overriding2 
+{
+    public static void main(String[] args) 
+    {
+        Car c=new LuxuryCar();
+        c.start();
+        c.accelerate();
+        c.changeGear();
+//        c.openRoof();
+    }
+    
+}
