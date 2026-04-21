b6d8fc9967ee6583019ccebbbdc4dfb826c9feb9
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-21 23:17:38 +0530

    Implement abstract class with subclass example

--- Java/Section16/AbstractExample.java
+++ Java/Section16/AbstractExample.java
@@ -1,1 +1,29 @@
-
+abstract class Super
+{
+    public Super()
+    {
+        System.out.println("Super Constructor");
+    }
+    public void method1()
+    {
+        System.out.println("Mehtod1 of Super");
+    }
+    abstract public void method2();
+}
+class Sub extends Super
+{
+    @Override
+    public void method2()
+    {
+        System.out.println("Sub method2");
+    }
+}
+public class AbstractExample 
+{
+    public static void main(String[] args) 
+    {
+        Super s=new Sub();
+        s.method1();
+        s.method2();
+    } 
+}
