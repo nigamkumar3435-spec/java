c40f8c002ca58e35bfa291f907e4b39268f5f90e
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-25 21:54:01 +0530

    Implement polymorphism with Super and Sub classes
    Added Super and Sub classes to demonstrate polymorphism.

--- Java/Sectiion15/Polymorphism2.java
+++ Java/Sectiion15/Polymorphism2.java
@@ -1,1 +1,26 @@
+class Super
+{
+    public void display()
+    {
+        System.out.println("Super Display");
+    }
+}
+class Sub extends Super
+{
+    public void display()
+    {
+        System.out.println("Sub Display");
+    }
+}
 
+public class Polymorphism2 
+{
+    public static void main(String[] args) 
+    {
+//        Super s1=new Super();
+//        s1.display();  //--> Super Display
+        
+        Super s2=new Sub();
+        s2.display();  //--> O/P:- Super Display & Sub Display
+    }   
+}
