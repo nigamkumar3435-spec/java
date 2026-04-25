fd0fbc199e94b6052cc4126d31d06260df992d3b
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-04-25 21:45:29 +0530

    Add method overloading for max function

--- Java/Sectiion15/Polymorphism1.java
+++ Java/Sectiion15/Polymorphism1.java
@@ -1,1 +1,22 @@
-
+class Overloading
+{
+    public int max(int a , int b)
+    {
+        return a>b?a:b;
+    }
+    public int max(int a, int b, int c)
+    {
+        if(a>b && a>c) return a;
+        else if(b>c) return b;
+        return c;
+    }
+}
+public class Polymorphism1 
+{
+    public static void main(String[] args) 
+    {
+        Overloading o1=new Overloading();
+        System.out.println(o1.max(10,5));
+        System.out.println(o1.max(10,12,6));
+    }    
+}
