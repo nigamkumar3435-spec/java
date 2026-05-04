ddafced9fae938950f8af1fe68187b736ca22bcb
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-04 23:47:26 +0530

    Implement interfaces for smartphone functionality

--- Java/Section17/InterfacePractice2.java
+++ Java/Section17/InterfacePractice2.java
@@ -1,1 +1,53 @@
-
+class Phone
+{
+    public void call()
+    {
+        System.out.println("Phone Call");
+    }
+    public void sms()
+    {
+        System.out.println("Phone Sending SMS");
+    }
+}
+interface ICamera
+{
+    void click();
+    void record();
+}
+interface IMusicPlayer
+{
+    void play();
+    void stop();
+}
+class SmartPhone extends Phone implements ICamera,IMusicPlayer
+{
+    public void videoCall()
+    {
+        System.out.println("Smart Phone video Calling");
+    }
+    public void click()
+    {
+        System.out.println("Smart Phone CLicking Photo");
+    }
+    public void record()
+    {
+        System.out.println("Smart Phone recording video");
+    }
+    public void play()
+    {
+        System.out.println("Smart Phone playing music");
+    }
+    public void stop()
+    {
+        System.out.println("Smart Phone stopped playing music");
+    }
+}
+public class InterfacePractice2 
+{
+    public static void main(String[] args) 
+    {
+        ICamera sp=new SmartPhone();
+        sp.click();
+        sp.record();
+    }    
+}
