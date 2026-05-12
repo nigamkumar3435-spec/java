c069cb0ea5d81a2dc4f6ee306ba96481194a6abc
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-12 20:11:45 +0530

    Add inner class implementation in InnerClass.java

--- Java/Section18/InnerClass.java
+++ Java/Section18/InnerClass.java
@@ -1,1 +1,28 @@
-
+class Outer
+{
+    int x=10;
+    class Inner
+    {
+        int y=20;
+        void innerDisplay()
+        {
+            System.out.println(x);
+            System.out.println(y);
+        }
+    }
+    void outerDisplay()
+    {
+        Inner i=new Inner();
+        i.innerDisplay();
+        System.out.println(i.y);
+    }
+}
+public class InnerClass 
+{
+    public static void main(String[] args) 
+    {
+        Outer o=new Outer();
+        o.outerDisplay();
+        Outer.Inner i=new Outer().new Inner();
+    }  
+}
